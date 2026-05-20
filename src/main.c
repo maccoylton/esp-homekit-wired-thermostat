d95848813f5e8b260b0351ae85c7a710a935b1f2
David Brown <dave@Mac.maccoylton.com>
2026-05-20 16:39:51 +0100

    v1.1.1-pre - log_level_set: handle float/int/uint8 formats

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 #define DEVICE_NAME "wired-thermostat"
 #define DEVICE_MODEL "2"
 #define DEVICE_SERIAL "12345678"
-#define FW_VERSION "1.1"
+#define FW_VERSION "1.1.1"
 
 #include <stdio.h>
 #include <espressif/esp_wifi.h>
