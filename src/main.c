4c3b41ef57df662da83ff0e47393d5becc561028
David Brown <dave@Mac.maccoylton.com>
2026-05-20 16:01:55 +0100

    v1.1
    - printf -> LOG() conversion across main.c
    - lcm_emergency, preserve_state, log_level_ch characteristics added
    - SAVE_DELAY 1000 -> 3000
    - Components moved to workspace-level (../../components/)
    - Removed submodule components

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 #define DEVICE_NAME "wired-thermostat"
 #define DEVICE_MODEL "2"
 #define DEVICE_SERIAL "12345678"
-#define FW_VERSION "1.0"
+#define FW_VERSION "1.1"
 
 #include <stdio.h>
 #include <espressif/esp_wifi.h>
@@ -51,7 +51,7 @@
 
 #include <ota-api.h>
 
-const int SAVE_DELAY = 1000;
+const int SAVE_DELAY = 3000;
 
 homekit_characteristic_t wifi_check_interval   = HOMEKIT_CHARACTERISTIC_(CUSTOM_WIFI_CHECK_INTERVAL, 10, .setter=wifi_check_interval_set);
 /* checks the wifi is connected and flashes status led to indicated connected */
@@ -59,6 +59,10 @@ homekit_characteristic_t task_stats   = HOMEKIT_CHARACTERISTIC_(CUSTOM_TASK_STAT
 homekit_characteristic_t wifi_reset   = HOMEKIT_CHARACTERISTIC_(CUSTOM_WIFI_RESET, false, .setter=wifi_reset_set);
 homekit_characteristic_t ota_beta     = HOMEKIT_CHARACTERISTIC_(CUSTOM_OTA_BETA, false, .setter=ota_beta_set);
 homekit_characteristic_t lcm_beta    = HOMEKIT_CHARACTERISTIC_(CUSTOM_LCM_BETA, false, .setter=lcm_beta_set);
+homekit_characteristic_t lcm_emergency = HOMEKIT_CHARACTERISTIC_(CUSTOM_LCM_EMERGENCY, false, .setter=lcm_emergency_set);
+
+homekit_characteristic_t preserve_state   = HOMEKIT_CHARACTERISTIC_(CUSTOM_PRESERVE_STATE, false, .setter=preserve_state_set);
+homekit_characteristic_t log_level_ch     = HOMEKIT_CHARACTERISTIC_(CUSTOM_LOG_LEVEL, 2, .setter=log_level_set);
 
 homekit_characteristic_t ota_trigger  = API_OTA_TRIGGER;
 homekit_characteristic_t name         = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
@@ -96,7 +100,7 @@ int led_off_value = -1;
 uint8_t uart_port=0;
 
 void set_current_state(){
-    printf ("%s: target state: %d, current state: %d, target temp: %2.1f, current temp:%2.1f ", __func__, target_state.value.int_value, current_state.value.int_value, target_temperature.value.float_value ,current_temperature.value.float_value);
+    LOG(LOG_EVENT, "%s: target state: %d, current state: %d, target temp: %2.1f, current temp:%2.1f\n", __func__, target_state.value.int_value, current_state.value.int_value, target_temperature.value.float_value ,current_temperature.value.float_value);
     switch (target_state.value.int_value){
         case 0: /* off */
             current_state.value = HOMEKIT_UINT8 (0);
@@ -114,7 +118,7 @@ void set_current_state(){
         default:
             break;
     }
-    printf ("New current state: %d\n", current_state.value.int_value);
+    LOG(LOG_EVENT, "%s: New current state: %d\n", __func__, current_state.value.int_value);
     sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
     homekit_characteristic_notify(&current_state, current_state.value);
 }
@@ -122,7 +126,7 @@ void set_current_state(){
 
 void set_external_temp_sensor (homekit_value_t value){
     
-    printf ("%s: %s", __func__, value.bool_value ? "true" : "false");
+    LOG(LOG_EVENT, "%s: %s\n", __func__, value.bool_value ? "true" : "false");
     externalTempSensor = value.bool_value;
     sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
     if (externalTempSensor == true) {
@@ -130,20 +134,20 @@ void set_external_temp_sensor (homekit_value_t value){
     } else {
         tuya_thermostat_emitChange (CHANGE_TYPE_INTERNAL_TEMP);
     }
-    printf ("%s: end\n", __func__);
+    LOG(LOG_FLOW, "%s: end\n", __func__);
     
 }
 
 void set_target_temperature (homekit_value_t value){
     /* called when the IOS device updates the value */
-    printf ("%s: %2.1f ", __func__, value.float_value);
+    LOG(LOG_EVENT, "%s: %2.1f\n", __func__, value.float_value);
     target_temperature.value = HOMEKIT_FLOAT(value.float_value);
     homekit_characteristic_bounds_check(&target_temperature);
     sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
     set_current_state();
     /* save routine will also notify MCU of new target temperature to avoid bounce */
 
-    printf (" target temperature: %2.1f\n", target_temperature.value.float_value);
+    LOG(LOG_EVENT, "%s: target temperature: %2.1f\n", __func__, target_temperature.value.float_value);
     homekit_characteristic_notify(&target_temperature, target_temperature.value);
     /* if the target temperature has changed then the current state may have changed */
     
@@ -152,23 +156,23 @@ void set_target_temperature (homekit_value_t value){
 
 void set_target_state (homekit_value_t value){
     /* called when the IOS device updates the value */
-    printf ("%s: ", __func__);
+    LOG(LOG_ACTION, "%s\n", __func__);
     switch ( (int) value.int_value)
     {
         case 0: /* off */
             tuya_thermostat_setPower( false , true);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
-            printf (" Off \n");
+            LOG(LOG_ACTION, "%s: Off\n", __func__);
             break;
         case 1: /* heat */
             tuya_thermostat_setPower( true , true);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
-            printf (" Heat\n");
+            LOG(LOG_ACTION, "%s: Heat\n", __func__);
             break;
         case 2: /* cool */
         case 3: /* auto */
         default:
-            printf (" Unsuported\n");
+            LOG(LOG_ERR, "%s: Unsuported\n", __func__);
     }
     
     target_state.value = HOMEKIT_UINT8(value.int_value);
@@ -179,7 +183,7 @@ void set_target_state (homekit_value_t value){
 
 void gpio_init() {
     
-    printf("Initialising buttons\n");
+    LOG(LOG_ACTION, "%s: Initialising buttons\n", __func__);
     
 }
 
@@ -187,17 +191,17 @@ void gpio_init() {
 void tuya_thermostat_emitChange(TUYA_Thermostat_change_type_t cmd)
 {
     
-    printf ("%s: emit command : ", __func__);
+    LOG(LOG_EVENT, "%s: emit command\n", __func__);
     switch (cmd){
         case CHANGE_TYPE_POWER:
-            printf (" power on: %d, target state:%d\n", powerOn, target_state.value.int_value);
+            LOG(LOG_EVENT, "%s: power on: %d, target state:%d\n", __func__, powerOn, target_state.value.int_value);
             target_state.value = HOMEKIT_UINT8(powerOn ? 1 : 0);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
             homekit_characteristic_notify(&target_state, target_state.value);
             set_current_state();
             break;
         case CHANGE_TYPE_SETPOINT_TEMP:
-            printf (" setPoint: %f, target temperature: %f\n", setPointTemp, target_temperature.value.float_value);
+            LOG(LOG_EVENT, "%s: setPoint: %f, target temperature: %f\n", __func__, setPointTemp, target_temperature.value.float_value);
             target_temperature.value = HOMEKIT_FLOAT(setPointTemp);
             homekit_characteristic_bounds_check(&target_temperature);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
@@ -205,7 +209,7 @@ void tuya_thermostat_emitChange(TUYA_Thermostat_change_type_t cmd)
             set_current_state();
             break;
         case CHANGE_TYPE_INTERNAL_TEMP:
-            printf (" internalTemp: %f, current temperature: %f\n", internalTemp, current_temperature.value.float_value);
+            LOG(LOG_EVENT, "%s: internalTemp: %f, current temperature: %f\n", __func__, internalTemp, current_temperature.value.float_value);
             current_temperature.value = HOMEKIT_FLOAT(internalTemp);
             homekit_characteristic_bounds_check(&current_temperature);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
@@ -213,19 +217,19 @@ void tuya_thermostat_emitChange(TUYA_Thermostat_change_type_t cmd)
             set_current_state();
             break;
         case CHANGE_TYPE_MODE:
-            printf (" mode: unsupported\n");
+            LOG(LOG_ERR, "%s: mode: unsupported\n", __func__);
             break;
         case CHANGE_TYPE_ECONOMY:
-            printf (" economy: unsupported\n");
+            LOG(LOG_ERR, "%s: economy: unsupported\n", __func__);
             break;
         case CHANGE_TYPE_LOCK:
-            printf (" lock: unsupported\n");
+            LOG(LOG_ERR, "%s: lock: unsupported\n", __func__);
             break;
         case CHANGE_TYPE_SCHEDULE:
-            printf (" schedule: unsupported\n");
+            LOG(LOG_ERR, "%s: schedule: unsupported\n", __func__);
             break;
         case CHANGE_TYPE_EXTERNAL_TEMP:
-            printf (" current temperature: %f\n", externalTemp);
+            LOG(LOG_EVENT, "%s: current temperature: %f\n", __func__, externalTemp);
             current_temperature.value = HOMEKIT_FLOAT(externalTemp);
             homekit_characteristic_bounds_check(&current_temperature);
             sdk_os_timer_arm (&save_timer, SAVE_DELAY, 0 );
@@ -233,7 +237,7 @@ void tuya_thermostat_emitChange(TUYA_Thermostat_change_type_t cmd)
             set_current_state();
             break;
         default:
-            printf (" %s Unknow change type received from MCU\n", __func__);
+            LOG(LOG_ERR, "%s: Unknow change type received from MCU\n", __func__);
     }
 }
 
@@ -261,10 +265,13 @@ homekit_accessory_t *accessories[] = {
 //            &heating_threshold,
             &units,
             &external_temp_sensor,
+            &preserve_state,
+            &log_level_ch,
             &ota_trigger,
             &wifi_reset,
             &ota_beta,
             &lcm_beta,
+            &lcm_emergency,
             &task_stats,
             &wifi_check_interval,
             NULL
@@ -284,7 +291,7 @@ homekit_server_config_t config = {
 
 void recover_from_reset (int reason){
     /* called if we restarted abnormally */
-    printf ("%s: reason %d\n", __func__, reason);
+    LOG(LOG_ACTION, "%s: reason %d\n", __func__, reason);
     load_characteristic_from_flash(&target_temperature);
     homekit_characteristic_notify (&target_temperature, target_temperature.value);
     tuya_thermostat_setSetPointTemp (target_temperature.value.float_value, true);
@@ -297,10 +304,11 @@ void recover_from_reset (int reason){
 
 void save_characteristics ( ){
     
-    printf ("%s:\n", __func__);
+    LOG(LOG_EVENT, "%s:\n", __func__);
     save_characteristic_to_flash (&target_temperature, target_temperature.value);
     save_characteristic_to_flash (&target_state, target_state.value);
     save_characteristic_to_flash (&external_temp_sensor, external_temp_sensor.value);
+    save_characteristic_to_flash(&preserve_state, preserve_state.value);
     save_characteristic_to_flash(&wifi_check_interval, wifi_check_interval.value);
     
     /* do this hear to stop temperature bounce */
